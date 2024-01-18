e4bc712b8b3c7b5d3fcf5b052c075d01368bfaa8
saxter119 <saiterbellomateo@gmail.com>
2024-01-18 10:06:57 -0400

    preview get and create contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { AgendaModule } from './Agenda/agenda.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgendaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
